54891fea7492e93febc64f877beb92724e994a52
bduvignaud <benoit.duvignaud@gmail.com>
2020-08-12 09:41:22 +0200

    Add the possibility to give a people quantity precision

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,15 @@
 class RecipesController < ApplicationController
   def index
+    @people_quantity = Recipe.pluck(:people_quantity).uniq.compact.sort
   end
 
   def show
     search_input = params["ingredient"].join(" ").downcase
 
     @recipes = Recipe.search_by_ingredient(search_input)
+
+    if params['people'].present?
+      @recipes = @recipes.where(people_quantity: params['people'])
+    end
   end
 end
